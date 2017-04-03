790c96319a4692782fd929a6ef670fcb203138ea
singh1114 <ranvir.singh1114@gmail.com>
2017-04-04 03:54:11 +0530

    Fixes: #1

--- eventApp/admin.py
+++ eventApp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import EventList, MemberList, ParticipantsList
+from .models import EventList, MemberList
 # Register your models here.
 
 admin.site.register(EventList)
 admin.site.register(MemberList)
-admin.site.register(ParticipantsList)
